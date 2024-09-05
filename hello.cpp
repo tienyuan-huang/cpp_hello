16fbeb74fdfca9edc0711ef5982389b04ea1928e
Evan Huang <evan.huang@axiomtek.com.tw>
2024-09-05 15:22:32 +0800

    test

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,31 @@
+// `hello` source code
+
 #include <iostream>
 
+// add a foo function to print layer
+void foo() {
+  std::cout << "layerlayerlayerlayerlayerlayer" << std::endl;
+  std::cout << "layerlayerlayerlayerlayerlayer" << std::endl;
+  std::cout << "\n" << std::endl;
+  std::cout << "foo" << std::endl;
+  std::cout << "foofoofoofoofoofoofoofoofoofoo" << std::endl;
+}
+
+// make input int double
+int bar(int x) {
+  return x * 2;
+}
+
 int main() {
-  std::cout << "Hello, World! I'm Evan, and this is q5." << std::endl;
-  std::cout << "this file is in the `meta-hello` layer." << std::endl;
-  std::cout << "Just for SDK example" << std::endl;
+  std::cout << "Hello, World! I'm Evan, and this is q6.\n" << std::endl;
+  foo();
+
+  // print x
+  int x = 50;
+  std::cout << "x = " << x << std::endl;
+  std::cout << "start double x......" << std::endl;
+  x = bar(50);
+  std::cout << "x = " << x << std::endl;
+
   return 0;
 }

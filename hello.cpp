8d54660ff4f129c74fe228d8fe18d668a4be2d7b
Evan Huang <evan.huang@axiomtek.com.tw>
2024-09-05 11:47:36 +0800

    first commit

---
+++ hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hello, World! I'm Evan, and this is q5." << std::endl;
+  std::cout << "this file is in the `meta-hello` layer." << std::endl;
+  std::cout << "Just for SDK example" << std::endl;
+  return 0;
+}
